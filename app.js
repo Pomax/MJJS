76c64654f2cff32633f0d48684b247c74884e16f
Mike Kamermans <pomax@nihongoresources.com>
2013-07-19 16:54:44 -0700

    better ghpage support

--- app.js
+++ app.js
@@ -4,21 +4,7 @@ var http = require("http"),
     app = express(),
     nunjucksEnv = new nunjucks.Environment(new nunjucks.FileSystemLoader('views'));
 nunjucksEnv.express(app);
-
-app.get("/", function(req, res) {
-  res.render("public/index.html");
-});
-
-var js = function(_, res, next) {
-  res.header('Content-Type', 'application/javascript');
-  next();
-};
-
 app.use(express.static('public'));
-
-app.use(js);
-app.use('/src', express.static('src'));
-
 app.listen(3000, function() {
   console.log('Express server listening on port 3000');
 });
